43f0ecef2ec313d592973b88ea83d70001f83c00
DucAnh28 <ducanhnguyeny2k@gmail.com>
2022-10-20 10:30:31 +0700

    done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'AppRating';
+  alo = '';
+  done(number:any){
+    if (number>7){
+      this.alo = "tot";
+    }
+    else if (number>5){
+      this.alo = "bthuong";
+    }
+    else if (number>3){
+      this.alo = "Trung binh";
+    }
+    else {
+      this.alo = "te";
+    }
+  }
 }
